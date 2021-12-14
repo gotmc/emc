f41bc2cc930421223189e919b8f84ac151c089ba
Matthew Rankin <matthew@cumulusware.com>
2021-12-14 06:23:20 -0600

    Increase testing

--- cable/cable_test.go
+++ cable/cable_test.go
@@ -13,33 +13,44 @@ import (
 
 func TestLoss(t *testing.T) {
 	var tests = []struct {
-		cable  CableType
-		length float64
-		want   []float64
+		cable    CableType
+		length   float64
+		wantFreq []float64
+		wantLoss []float64
 	}{
 		{
-			cable:  LMR400,
-			length: 50.0,
-			want:   []float64{0.0056, 0.0059},
+			cable:    LMR400,
+			length:   50.0,
+			wantLoss: []float64{0.0056, 0.0059},
+		},
+		{
+			cable:  RG58,
+			length: 100.0,
+			wantFreq: []float64{
+				1.00e6, 1.00e7, 5.00e7, 1.00e8, 2.00e8, 4.00e8, 7.00e8, 9.00e8, 1.00e9,
+			},
+			wantLoss: []float64{0.4, 1.4, 3.3, 4.9, 7.3, 11.2, 16.9, 20.1, 21.5},
 		},
 	}
 	for _, test := range tests {
 		name := fmt.Sprintf("%s at %f.2 ft", test.cable, test.length)
 		t.Run(name, func(t *testing.T) {
 			_, gotLoss := Loss(test.cable, test.length)
-			assertFloat64(t, name, gotLoss[0], test.want[0], 0.1e-6)
+			assertFloat64(t, name, gotLoss[0], test.wantLoss[0], 0.1e-6)
 		})
 	}
 }
 
-func assert(t *testing.T, label string, got, want interface{}) {
+func assert(tb testing.TB, label string, got, want interface{}) {
+	tb.Helper()
 	if got != want {
-		t.Errorf("\ngot  = `%#v` for %s\nwant = `%#v`", got, label, want)
+		tb.Errorf("\ngot  = `%#v` for %s\nwant = `%#v`", got, label, want)
 	}
 }
 
-func assertFloat64(t *testing.T, label string, got, want, tolerance float64) {
+func assertFloat64(tb testing.TB, label string, got, want, tolerance float64) {
+	tb.Helper()
 	if diff := math.Abs(want - got); diff >= tolerance {
-		t.Errorf("\ngot %s  = %#v \t\nwant %s = %#v", label, got, label, want)
+		tb.Errorf("\ngot %s  = %#v \t\nwant %s = %#v", label, got, label, want)
 	}
 }
